ba4b84cec647e5385172e7c324f908b1dfb8580d
HeleneBrandt <144004013+HeleneBrandt@users.noreply.github.com>
2024-04-02 14:19:59 +0200

    Add index.html file KinoBilletter

--- Oblig2/src/main/java/kinoprogram/oblig2/HomeController.java
+++ Oblig2/src/main/java/kinoprogram/oblig2/HomeController.java
@@ -1,13 +1,5 @@
 package kinoprogram.oblig2;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HomeController {
-    @GetMapping("/")
-    public String heiVerden(String navn) {
-        return "Hei verden" + navn;
-        // kommer til prot 8080 når kjøres da må man bruke chrome.
-    }
-}
+
+
